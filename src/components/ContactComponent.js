2f407c8c1e83aad7d4dd2750568aec06ca6ba73f
placidohoff <67982091+placidohoff@users.noreply.github.com>
2021-10-20 20:24:28 -0400

    Added error text to redux contact form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from 'react'
 import {
     Breadcrumb, BreadcrumbItem, Button, Col, Row, Label
 } from 'reactstrap'
 import { Link } from 'react-router-dom'
-import { Control, LocalForm } from 'react-redux-form'
+import { Control, LocalForm, Errors } from 'react-redux-form'
+
+//Test to see if a value exists and has a length:
+const required = val => val && val.length
+//Test if the  doen't exist and is or is less than the maximum length
+const maxLength = len  => val => !val || (val.length <= len)
+//Test if the value exist and is greater than the minimum length:
+const minLength = len => val => (val && val >= len)
+
+const isNumber = val => !isNaN(+val)
+
+const validEmail = val => /^[A-Z0-9.%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i.test(val)
 
 class Contact extends React.Component {
     constructor(props) {
@@ -24,9 +35,9 @@ class Contact extends React.Component {
             }
         }
 
-        
+
     }
-     
+
 
     handleSubmit(values) {
         console.log("Current state is: " + JSON.stringify(values))
@@ -83,13 +94,29 @@ class Contact extends React.Component {
                         <hr />
                     </div>
                     <div className="col-md-10">
-                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Control.text model=".firstName" id="firstName" name="firstName"
                                         placeholder="First Name"
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".firstName"
+                                        show="touched"
+                                        component="div"
+                                        messages= {{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
                                     />
                                 </Col>
                             </Row>
@@ -99,6 +126,22 @@ class Contact extends React.Component {
                                     <Control.text model=".lastName" id="lastName" name="lastName"
                                         placeholder="Last Name"
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".lastName"
+                                        show="touched"
+                                        component="div"
+                                        messages= {{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
                                     />
                                 </Col>
                             </Row>
@@ -108,6 +151,24 @@ class Contact extends React.Component {
                                     <Control.text model=".phoneNum" id="phoneNum" name="phoneNum"
                                         placeholder="Phone number"
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(10),
+                                            maxLength: maxLength(15),
+                                            isNumber
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".phoneNum"
+                                        show="touched"
+                                        component="div"
+                                        messages= {{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 10 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }}
                                     />
                                 </Col>
                             </Row>
@@ -117,11 +178,24 @@ class Contact extends React.Component {
                                     <Control.text model=".email" id="email" name="email"
                                         placeholder="Email"
                                         className="form-control"
+                                        validators={{
+                                            required,
+                                            validEmail
+                                        }}
+                                    />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".email"
+                                        show="touched"
+                                        component="div"
+                                        messages= {{
+                                            required: 'Required',
+                                            validEmail: 'Invalid email address'                                        }}
                                     />
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Col md={{size: 4, offset: 2}}>
+                                <Col md={{ size: 4, offset: 2 }}>
                                     <div className="form-check">
                                         <Label check>
                                             <Control.checkbox
@@ -151,7 +225,7 @@ class Contact extends React.Component {
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Col md={{size: 10, offset: 2}}>
+                                <Col md={{ size: 10, offset: 2 }}>
                                     <Button type="submit" color="primary">
                                         Send Feedback
                                     </Button>
