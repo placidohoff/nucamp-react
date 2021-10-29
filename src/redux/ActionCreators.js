4fb44024fa1f12f867acc7893cc0e731d7947f70
placidohoff <67982091+placidohoff@users.noreply.github.com>
2021-10-28 23:34:55 -0400

    Added redux middlewares: thunk and logger

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes'
+import { CAMPSITES } from '../shared/campsites'
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,29 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author,
         text
     }
+})
+
+//REDUX-THUNK SYNTAX: We run one function then the next
+    //this is essentially nesting an arrow funciton within another arrow function
+        //only possible via redux-thunk
+export const fetchCampsites = () => dispatch => {
+    dispatch(campsitesLoading())
+
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES))
+    },2000)
+}
+
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+})
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+})
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
 })
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 //IMPORT EACH INDIVIDUAL REDUCER TO COMBINE THEM INTO ONE:
 import { Campsites } from './campsites'
 import { Comments, COMMENTS } from './comments'
@@ -12,7 +14,9 @@ export const ConfigureStore = () => {
             comments: Comments,
             partners: Partners,
             promotions: Promotions
-        })
+        }),
+
+        applyMiddleware(thunk, logger)
     );
 
     return store;
